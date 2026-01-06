70fff0b48ab4290f90e17a4b182a5c15a914e31b
Saumya Pandey <143836876+saumyapandey234@users.noreply.github.com>
2026-01-06 15:10:03 +0530

    Delete SbPortfolio directory

--- SbPortfolio/src/main/java/SBPortfolio/Repo/contactRepo.java
+++
@@ -1,7 +0,0 @@
-package SBPortfolio.Repo;
-
-import SBPortfolio.Entity.Contact;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface contactRepo extends JpaRepository<Contact, Long> {
-}
